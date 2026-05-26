ca25669c1fe4752b1f5ce4975e4b25a7c235f20f
Maxrc <maxwell.r.coblenz@gmail.com>
2026-05-26 11:50:06 -0400

    Adding a template html file, and starting other funcitonalities for the blog

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,6 @@
-from django.contrib import admin
 from django.urls import path, include
 from . import views
 
 urlpatterns = {
-    path('admin/', admin.site.urls),
-    path('', include('blog.urls')),
     path('', views.PostListView.as_view(), name='post-list'),
 }
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
+from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
 
@@ -11,3 +13,28 @@ class PostListView(ListView):
 
     def get_queryset(self):
         return Post.objects.filter(is_public=True)
+    
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
+    context_obect_name = 'post'
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    template_name = 'blog/postform.html'
+    fields = ['title', 'is_public', 'tags', 'image']
+    
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin, UpdateView):
+    model = Post
+    template_name = 'blog/post_form.html'
+    fields = ['title', 'is_public', 'tags', 'image']
+
+class PostDeleteView(LoginRequiredMixin, DeleteView):
+    model = Post
+    template_name = 'blog/post_confirm/delete.html'
+    success_url = reverse_lazy('post-list')
+    
